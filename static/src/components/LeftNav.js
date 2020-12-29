7059621ec027c36cc2d77f5401de8c819ba84cae
minqi <minqijiang@fb.com>
2020-12-29 16:59:12 +0000

    add google scholar

--- static/src/components/LeftNav.js
+++ static/src/components/LeftNav.js
@@ -17,7 +17,8 @@ class LeftNav extends Component {
 					</div>
 					<ul className='links'>
 						<li key='twitter'><div className='icon twitter-icon'/> <a target='__blank' href='https://www.twitter.com/minqijiang'>@MinqiJiang</a></li>
-						<li key='github'><div className='icon git-icon'/> <a target='__blank' href='https://github.com/minqi'>minqi</a></li>
+						<li key='github'><div className='icon git-icon'/> <a target='__blank' href='https://github.com/minqi'>GitHub</a></li>
+						<li key='scholar' className='scholar-link'><div className='icon scholar-icon'/> <a target='__blank' href='https://scholar.google.com/citations?user=b7LglM0AAAAJ'>Google Scholar</a></li>
 						<li key='email'><div className='icon email-icon'/> <span className='email'>msj at fb dot com</span></li>
 					</ul>
 				</div>
